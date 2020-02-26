228cb9274fe98886da5e6efac0d6e8096c1dfa67
Jesse Foster <jessefoster33@gmail.com>
2020-02-26 15:39:15 -0700

    Commit staged files, full project ported to GitHub.

---
+++ Assets/UdacityVR/Scripts/HeadRotation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadRotation : MonoBehaviour {
+
+	// Use this for initialization
+    // Line 10 enables gyro
+	void Start () {
+        Input.gyro.enabled = true;
+	}
+	// Line 16 stores rotation data from gyro
+    // Line 17 ensures rotation matches head movements by flipping gyro rotation about the z-axis and then rotate around x-axis
+	// Update is called once per frame
+	void Update () {
+        Quaternion att = Input.gyro.attitude;
+        att = Quaternion.Euler(90f, 0f, 0f) * new Quaternion(att.x, att.y, -att.z, -att.w);
+        transform.rotation = att; //this assigns rotation to camera
+	}
+}
